86e31aadce5982a0e825de0a2ccb95b98f4d4aad
maxroseinnes <mrose5365@student.pps.net>
2022-02-01 15:16:26 -0800

    Didn't really do much, just set things up and learned about neural networks

---
+++ src/FinalProjectExperimentation.java
@@ -0,0 +1,9 @@
+public class FinalProjectExperimentation {
+    public static void main(String[] args) {
+        new FinalProjectExperimentation();
+    }
+
+    public FinalProjectExperimentation() {
+
+    }
+}
---
+++ src/NeuralNetwork.java
@@ -0,0 +1,5 @@
+public class NeuralNetwork {
+    public NeuralNetwork(int[] nodeCounts) {
+
+    }
+}

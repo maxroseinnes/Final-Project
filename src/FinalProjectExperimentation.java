bc53e7f1951043a43411154f935d6198e211bd02
maxroseinnes <mrose5365@student.pps.net>
2022-02-09 14:31:46 -0800

    Starting writing code for a genetic algorithm

--- src/FinalProjectExperimentation.java
+++ src/FinalProjectExperimentation.java
@@ -4,6 +4,36 @@ public class FinalProjectExperimentation {
     }
 
     public FinalProjectExperimentation() {
+        Perceptron[] agents = new Perceptron[1000000];
+        int[] scores = new int[agents.length];
 
+        for (int i = 0; i < agents.length; i++) {
+            agents[i] = new Perceptron(2, 0, 0);
+        }
+
+        for (int i = 0; i < agents.length; i++) {
+            int score = 0;
+            for (int j = 0; j < 100; j++) {
+                double[] inputs = new double[2];
+                for (int k = 0; k < inputs.length; k++) {
+                    inputs[k] = Math.random();
+                }
+                int guess = (int) agents[i].feedForward(inputs);
+                int target = inputs[0] >= inputs[1] ? 1 : -1;
+
+                if (guess == target) {
+                    score++;
+                }
+            }
+
+            scores[i] = score;
+        }
+
+        int bestIndex = 0;
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i] > scores[bestIndex]) {
+                bestIndex = i;
+            }
+        }
     }
 }
--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,21 +1,50 @@
 public class Perceptron {
-    double weight1, weight2, bias;
+    double[] weights;
+    double bias;
+    double mutationChance;
+    double mutationAmount;
 
-    public Perceptron() {
-        this.weight1 = 2 * Math.random() - 1;
-        this.weight2 = 2 * Math.random() - 1;
+    public Perceptron(int inputCount, double mutationChance, double mutationAmount) {
+        weights = new double[inputCount];
+        for (int i = 0; i < inputCount; i++) {
+            weights[i] = 2 * Math.random() - 1;
+        }
         this.bias = 2 * Math.random() - 1;
+        this.mutationChance = mutationChance;
+        this.mutationAmount = mutationAmount;
     }
 
-    double feedForward(double input1, double input2) {
-        return sigmoid(input1 * weight1 + input2 * weight2 + bias);
+    double feedForward(double[] inputs) {
+        double sum = bias;
+        for (int i = 0; i < weights.length; i++) {
+            sum += inputs[i] * weights[i];
+        }
+        return sum;
     }
 
-    void mutate() {
+    Perceptron mutate() {
+        Perceptron toReturn = new Perceptron(weights.length, mutationChance, mutationAmount);
 
+        for (int i = 0; i < weights.length; i++) {
+            toReturn.weights[i] = weights[i];
+            if (Math.random() < mutationChance) {
+                toReturn.weights[i] += (2 * Math.random() * mutationAmount) - mutationAmount;
+            }
+        }
+
+        toReturn.bias = bias;
+        if (Math.random() < mutationChance) {
+            toReturn.bias += (2 * Math.random() * mutationAmount) - mutationAmount;
+        }
+
+        return toReturn;
     }
 
     double sigmoid(double input) {
         return Math.exp(input) / (Math.exp(input) + 1);
+    }
+
+    int sign(double input) {
+        return input >= 0 ? 1 : -1;
     }
 }

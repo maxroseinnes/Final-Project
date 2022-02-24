e29f9de854a9d7af5666ffe894242f82cc4ec1ae
maxroseinnes <mrose5365@student.pps.net>
2022-02-24 15:30:43 -0800

    verified that my code works

--- src/FinalProjectExperimentation.java
+++ src/FinalProjectExperimentation.java
@@ -6,16 +6,12 @@ public class FinalProjectExperimentation {
     }
 
     public FinalProjectExperimentation() {
-        ThreeLayerNeuralNetwork network = new ThreeLayerNeuralNetwork(2, 2, 1, true);
-        network.printContents();
+        ThreeLayerNeuralNetwork network = new ThreeLayerNeuralNetwork(2, 3, 1, true);
 
         Matrix inputs = new Matrix(network.inputLayerNeurons, 1);
         for (int i = 0; i < inputs.getRows(); i++) {
             inputs.setValue(Util.map(Math.random(), 0, 1, -1, 1), i, 0);
         }
         Matrix output = network.feedForward(inputs);
-
-        inputs.printContents();
-        output.printContents();
     }
 }
